c9aefac70636f98bf785b32403693930b66ae704
giovannimaffeo <giovanni.maffeo@poli.ufrj.br>
2024-06-09 10:51:45 -0300

    chore: reorganize files structure

--- input.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT_HPP
-#define INPUT_HPP
-
-#include <vector>
-
-void input(const char *filename, std::vector<int> &profits, std::vector<int> &weights, std::vector<std::vector<int>> &forfeit_pairs, int &num_items, int &num_pairs, int &knapsack_size);
-
-#endif /* INPUT_HPP */
--- lcr.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LCR_HPP
-#define LCR_HPP
-
-#include <vector>
-
-LinkedList lcr(LinkedList& solution_line, const std::vector<int>& profits, const std::vector<int>& weights, float alpha);
-
-#endif /* LCR_HPP */
